8bac94165a9c2c2608d6c6e8015b48d3231ca645
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2024-02-05 21:59:26 +0100

    Java Spring Project

---
+++ src/main/java/com/example/pathfinder/model/dto/UserRegistrationDTO.java
@@ -0,0 +1,73 @@
+package com.example.pathfinder.model.dto;
+
+public class UserRegistrationDTO {
+
+    private String username;
+    private String fullname;
+    private String email;
+    private int age;
+    private String password;
+    private String confirmPassword;
+
+    public UserRegistrationDTO(){}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRegistrationDTO{" +
+                "username='" + username + '\'' +
+                ", fullname='" + fullname + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", password='" + password + '\'' +
+                ", confirmPassword='" + confirmPassword + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/example/pathfinder/web/AuthController.java
+++ src/main/java/com/example/pathfinder/web/AuthController.java
@@ -1,12 +1,25 @@
 package com.example.pathfinder.web;
 
+import com.example.pathfinder.model.dto.UserRegistrationDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
     @GetMapping("/register")
     public String register(){
       return "register";
+    }
+    @PostMapping ("/register")
+    public String doRegister(UserRegistrationDTO userREgistrationDTO){
+        System.out.println(userREgistrationDTO.toString());
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
     }
 }
